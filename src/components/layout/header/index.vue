706a3a2a0768f3cc643f960101fcec567a2a8c5a
explosionsinthesky <rend42@163.com>
2021-11-29 22:30:20 +0800

    temp

--- src/components/layout/header/index.vue
+++ src/components/layout/header/index.vue
@@ -1,47 +1,40 @@
 <template>
-  <div class="set-common-head-fixed-container">
-    <el-row class="common-head-fixed-container">
-      <el-col :span="24" class="show-pc">
-        <menu-bar></menu-bar>
-        <bread-crumb></bread-crumb>
-      </el-col>
-      <el-col :span="24" class="show-mobile">
-        <el-icon class="same-cell" size="30">
-          <expand @click="handleOpenMenu"/>
-        </el-icon>
-      </el-col>
-    </el-row>
+  <div class="header-container">
+    <div class="logo">内容管理系统后台</div>
   </div>
-
-  <el-drawer
-    v-model="drawer"
-    :direction="'rtl'"
-    :before-close="handleClose"
-    :with-header="false"
-    :size="290"
-  >
-    <menu-bar :direction-type="'vertical'" :show-logo="false" :show-search="false"></menu-bar>
-  </el-drawer>
 </template>
 
 <script setup lang="ts">
-  import { useRouter } from "vue-router";
-  import MenuBar from "@/components/layout/menu/MenuBar.vue";
-  import BreadCrumb from "@/components/layout/bread-crumb/index.vue";
-  import { ref } from "vue";
+import { useRouter } from "vue-router";
+import MenuBar from "@/components/layout/menu/MenuBar.vue";
+import BreadCrumb from "@/components/layout/bread-crumb/index.vue";
+import { ref } from "vue";
 
-  const router = useRouter()
+const router = useRouter()
 
-  const drawer = ref<boolean>(false)
-  const handleOpenMenu = () => {
-    drawer.value = !drawer.value
-  }
+const drawer = ref<boolean>(false)
+const handleOpenMenu = () => {
+  drawer.value = !drawer.value
+}
 
-  const handleClose = (done: any) => {
-    done()
-  }
+const handleClose = (done: any) => {
+  done()
+}
 
 </script>
 
 <style lang="scss" scoped>
+  .header-container {
+    position: relative;
+    height: 70px;
+    display: flex;
+    flex-direction: row;
+    background-color: #242f42;
+
+    .logo {
+      float: left;
+      width: 250px;
+      line-height: 70px;
+    }
+  }
 </style>
